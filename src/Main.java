a3196eef9b63127590104f5067965867aa9b9053
МАРК <markpan07@mail.ru>
2022-12-29 15:33:34 +0300

    1 task has done!

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,17 @@ public class Main {
 
     public static void task1 () {
         System.out.println("Задача 1");
+        char clientOS = 0;
+        switch (clientOS) {
+            case 0:
+                System.out.println("Установите версию приложения для iOS по ссылке");
+                break;
+            case 1:
+                System.out.println("Установите версию приложения для Android по ссылке");
+                break;
+            default:
+                System.out.println("У вас нет операционной системы :(");
+        }
     }
 
     public static void task2 () {
