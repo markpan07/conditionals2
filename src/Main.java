ea5648290903aa79b3ffad1c2c00eacbc0bddd13
МАРК <markpan07@mail.ru>
2022-12-29 17:10:31 +0300

    3 task has done!

--- src/Main.java
+++ src/Main.java
@@ -53,7 +53,23 @@ public class Main {
 
     public static void task3 () {
         System.out.println("Задача 3");
+        float year = 900;
+        boolean itIsForthYear = (year % 4) == 0;
+        boolean itIs400Year = (year % 400) == 0;
+        boolean itIs100Year = (year % 100) == 0;
+
+
+        if (itIsForthYear && !itIs100Year) {
+            System.out.println(year + " год вискосный");
+            } else if (itIs100Year && itIs400Year) {
+                System.out.println(year + " год вискосный");
+            } else {
+            System.out.println(year + " год обычный");
+        }
     }
+
+
+
 
     public static void task4 () {
         System.out.println("Задача 4");
